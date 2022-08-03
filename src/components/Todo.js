abb4a7af9b06fcaf808ae2aee338676064adfb14
Cloverblock <69807959+Cloverblock@users.noreply.github.com>
2022-08-03 17:46:58 +0300

    test commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -23,6 +23,7 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}) {
     if(edit.id) {
         return <TodoForm edit={edit} onSubmit={submitUpdate} />;
     }
+    //
 
   return todos.map((todo, index) => (
     <div
